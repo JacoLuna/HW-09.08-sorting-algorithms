4e7042a4c2f2676618508cb572a8bfacf88c996e
Jaco <jacoluna01@gmail.com>
2024-08-08 15:00:30 -0300

    bubble sorting and quick sorting done

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[4];
+        int[] array = new int[10];
         Random rnd = new Random();
 
         System.out.println("Original array");
         for (int i = 0; i < array.length; i++) {
-            array[i] = rnd.nextInt(1,100);
+            array[i] = rnd.nextInt(0,100) + 1;
             System.out.print(array[i] + " ");
         }
 
@@ -19,8 +19,16 @@ public class Main {
 //        selectionSort(array);
 //        printArray(array);
 
-        System.out.println("\nmerge sorted array");
-        mergeSort(array);
+//        System.out.println("\nmerge sorted array");
+//        mergeSort(array);
+//        printArray(array);
+
+//        System.out.println("\nquick sorted array");
+//        quickSort(array);
+//        printArray(array);
+
+        System.out.println("\nbubble sorted array");
+        quickSort(array);
         printArray(array);
     }
 
@@ -61,6 +69,13 @@ public class Main {
     //end selection sort
     //start bubble sort
     public static void bubbleSort(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j++) {
+                if (array[i] > array[j]){
+                    swap(array, i, j);
+                }
+            }
+        }
     }
     //end bubble sort
     //start merge sort
@@ -111,6 +126,33 @@ public class Main {
     //end merge sort
     //start quick sort
     public static void quickSort(int[] array){
+        quickSort(array, 0, array.length-1);
+    }
+    public static void quickSort(int[] array, int lowIndex, int highIndex){
+        if (lowIndex >= highIndex){
+            return;
+        }
+        int pivot = array[highIndex];
+        int leftPointer = lowIndex, rightPointer = highIndex;
+
+        while (leftPointer < rightPointer){
+            while (array[leftPointer] <= pivot && leftPointer < rightPointer){
+                leftPointer++;
+            }
+            while (array[rightPointer] >= pivot && leftPointer < rightPointer){
+                rightPointer--;
+            }
+            swap(array, leftPointer, rightPointer);
+        }
+        swap(array, leftPointer, highIndex);
+
+        quickSort(array, lowIndex, leftPointer-1);
+        quickSort(array, leftPointer+1, highIndex);
+    }
+    public static void swap(int[] array, int index1, int index2){
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
     }
     //end quick sort
 }
